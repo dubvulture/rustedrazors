754953f661e59641e0d1a61f98f0f306b7c003c1
Manuel Rota <manuel.rota@protonmail.ch>
2023-01-16 20:50:25 +0100

    refactor bench rw_ops! from macro to generic function

--- benches/src/main.rs
+++ benches/src/main.rs
@@ -22,67 +22,69 @@ impl Default for Payload {
     }
 }
 
-macro_rules! rw_ops {
-    ($r:ident, $w:ident) => {{
-        const RUNTIME: Duration = Duration::from_secs(5);
+fn rw_ops<R, W>(r: R, w: W) -> (f64, f64)
+where
+    R: Reader<Payload> + std::marker::Send + 'static,
+    W: Writer<Payload> + std::marker::Send + 'static,
+{
+    const RUNTIME: Duration = Duration::from_secs(5);
 
-        let barrier = Arc::new(Barrier::new(2));
+    let barrier = Arc::new(Barrier::new(2));
 
-        let r_handle = thread::spawn({
-            let barrier = Arc::clone(&barrier);
-            move || {
-                barrier.wait();
-                let start = Instant::now();
-                let mut value = Payload::default();
-                let mut iters: u64 = 0;
-                loop {
-                    black_box(value);
-                    $r.read(&mut value);
-                    iters += 1;
+    let r_handle = thread::spawn({
+        let barrier = Arc::clone(&barrier);
+        move || {
+            barrier.wait();
+            let start = Instant::now();
+            let mut value = Payload::default();
+            let mut iters: u64 = 0;
+            loop {
+                black_box(value);
+                r.read(&mut value);
+                iters += 1;
 
-                    let elapsed = Instant::now() - start;
-                    if elapsed > RUNTIME {
-                        break;
-                    }
+                let elapsed = Instant::now() - start;
+                if elapsed > RUNTIME {
+                    break;
                 }
-                iters
             }
-        });
-        let w_handle = thread::spawn({
-            let barrier = Arc::clone(&barrier);
-            move || {
-                barrier.wait();
-                let start = Instant::now();
-                let value = Payload::default();
-                let mut iters: u64 = 0;
-                loop {
-                    black_box(value);
-                    $w.write(value);
-                    iters += 1;
-
-                    let elapsed = Instant::now() - start;
-                    if elapsed > RUNTIME {
-                        break;
-                    }
-                }
-                iters
-            }
-        });
-
-        let r_res = r_handle.join();
-        let w_res = w_handle.join();
-
-        match (r_res, w_res) {
-            (Ok(r_iters), Ok(w_iters)) => {
-                let r_ops = r_iters as f64 / RUNTIME.as_secs() as f64 / 1000f64;
-                let w_ops = w_iters as f64 / RUNTIME.as_secs() as f64 / 1000f64;
-                (r_ops, w_ops)
-            }
-            _ => {
-                panic!("Something went wrong");
-            }
+            iters
         }
-    }};
+    });
+    let w_handle = thread::spawn({
+        let barrier = Arc::clone(&barrier);
+        move || {
+            barrier.wait();
+            let start = Instant::now();
+            let value = Payload::default();
+            let mut iters: u64 = 0;
+            loop {
+                black_box(value);
+                w.write(value);
+                iters += 1;
+
+                let elapsed = Instant::now() - start;
+                if elapsed > RUNTIME {
+                    break;
+                }
+            }
+            iters
+        }
+    });
+
+    let r_res = r_handle.join();
+    let w_res = w_handle.join();
+
+    match (r_res, w_res) {
+        (Ok(r_iters), Ok(w_iters)) => {
+            let r_ops = r_iters as f64 / RUNTIME.as_secs() as f64 / 1000f64;
+            let w_ops = w_iters as f64 / RUNTIME.as_secs() as f64 / 1000f64;
+            (r_ops, w_ops)
+        }
+        _ => {
+            panic!("Something went wrong");
+        }
+    }
 }
 
 fn bench_function(name: &'static str, fun: fn() -> (f64, f64)) {
@@ -94,10 +96,10 @@ fn bench_function(name: &'static str, fun: fn() -> (f64, f64)) {
 fn main() {
     bench_function("mutex", || {
         let (r, w) = mutex_spsc::new::<Payload>(Payload::default());
-        rw_ops!(r, w)
+        rw_ops(r, w)
     });
     bench_function("atomic", || {
         let (r, w) = atomic_spsc::new::<Payload>(Payload::default());
-        rw_ops!(r, w)
+        rw_ops(r, w)
     });
 }
